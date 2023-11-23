a34b2395082e9508d62c545b1d5da300e8b0fe87
Zakorko Serhii <rkot.vet@gmail.com>
2023-11-23 13:36:14 +0100

    begin of Main-page

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import classes from './Main.module.css'
 function Main(){
+    const navigate = useNavigate();
     return(
-        <div>
-            Main
-        </div>
+        <main>
+            <h2>Amazing Discounts on Garden Products!</h2>
+            <button className={classes.checkBtn} onClick={() => {
+                navigate("/sales");
+            }}>Check out</button>
+        </main>
     )
 }
 export default Main
