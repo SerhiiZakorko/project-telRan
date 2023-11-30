91b0da81a28f8e02f31948258764da460eef606b
Zakorko Serhii <rkot.vet@gmail.com>
2023-11-30 20:56:59 +0100

    page Not Found

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,8 +1,23 @@
-function NotFound(){
-    return(
-        <div>
-            404
-        </div>
-    )
+import classes from "../notFound/NotFound.module.css";
+import four from "../../assets/images/notFound/4.svg";
+import zero from "../../assets/images/notFound/0.svg";
+import { useNavigate } from "react-router-dom";
+function NotFound() {
+  const navigate = useNavigate();
+  return (
+    <section className={classes.notFoundPage}>
+      <div className={classes.errorWrapper}>
+        <img src={four} alt="4" />
+        <img src={zero} alt="0" />
+        <img src={four} alt="4" />
+      </div>
+      <div className={classes.messageWrapper}>
+        <h4>Page Not Found</h4>
+        <p>We’re sorry, the page you requested could not be found. <br/> Please go back
+          to the homepage.</p>
+        <button onClick={() => {navigate("/")}}>Go Home</button>
+      </div>
+    </section>
+  );
 }
-export default NotFound
+export default NotFound;
